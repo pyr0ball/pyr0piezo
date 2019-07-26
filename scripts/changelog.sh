83d4ba06536c27bbd056d8c018a7c9d176441fe8
pyr0ball <pyroballpcs@gmail.com>
2019-07-25 19:52:09 -0700

    added extra functions to changelog script, added mkdocs deploy

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -4,7 +4,15 @@
 #
 # Only commits tagged with #changelog will be added
 #
-# Usage: pass two tags in when running this script, and a changelog will be generated listing the differences between those two tags.
+# Usage: pass two tags or commit hashes in when running this script, and a changelog will be generated listing the differences between those two tags.
+# If args 3 & 4 are unused, default values will be substituted
 # ex:
 # ./changelog.sh rev.2.0.1 rev.2.1.1
-git log ${1}...${2} --pretty=format:'<li> <a href="https://github.com/pyr0ball/pyr0piezo/commit/%H">view commit &bull;</a> %s</li> ' --reverse | grep "#changelog"
+# ./changelog.sh b1d5817 02cd438 githun-username github project
+
+first-tag="${1}"
+last-tag="${2}"
+git-user="${3:-pyr0ball}"
+git-project="${4:-pyr0piezo}"
+
+git log ${first-tag}...${last-tag} --pretty=format:"%s [view commit](http://github.com/${git-user}/${git-project}/commit/%H) " --reverse | grep "#changelog"
---
+++ scripts/deploy-mkdocs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python -m mkdocs build
+cp docs/assets/images/Pyr0ball-logo.png docs/assets/images/favicon.png
