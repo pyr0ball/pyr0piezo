111d7cffe4eb5dbfdd1be5a1a941e324b4bac33a
pyr0ball <pyroballpcs@gmail.com>
2020-06-15 11:31:21 -0700

    fixed parsing error for git hash injection to version parameter #bugfix

--- firmware/AVR-Source/Pyr0_Piezo_Sensor_v2.x.x/version_git.py
+++ firmware/AVR-Source/Pyr0_Piezo_Sensor_v2.x.x/version_git.py
@@ -4,4 +4,6 @@ major_version = "2.3.1"
 
 git_head = str(subprocess.check_output(["git", "rev-parse", "HEAD"])[0:6])
 
+git_head = git_head.replace("b'", "'")
+
 print("-DPP_VERSION=\\\"" + major_version + "_" + git_head + "\\\"")
